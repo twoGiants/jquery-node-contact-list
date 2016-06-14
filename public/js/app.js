0e429d8e63570ddacc796f98ca98a9b69e77385b
Stanislav Jakuschevskij <Stas@2G.local>
2016-06-14 19:30:10 +0200

    view and save data to a json file implemented, remove element is next up

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,105 @@
 'use strict';
 
+// dependencies
 window.$ = window.jQuery = require('jquery');
-
 require('bootstrap');
-
 require('./../css/style.css');
+
+// Contact class
+function Contact(name, email, number) {
+    this.name   = name;
+    this.email  = email;
+    this.number = number;
+    this.id     = $.now();
+}
+
+// Main
+$(document).ready(documentReadyCb);
+
+function documentReadyCb() {
+    
+    // send data to back end
+    $('#submit-btn').click(submitBtnCb);
+    
+    refreshView();
+    
+    function submitBtnCb(event) {
+        var newContact = {};
+        var ajaxObj = {};
+        
+        newContact = new Contact(
+            $('input#new-name').val(), 
+            $('input#new-email').val(), 
+            $('input#new-number').val()
+        );
+        
+        ajaxObj = {
+            type: 'POST',
+            data: JSON.stringify(newContact),
+            contentType: 'application/json',
+            url: 'http://localhost:3000/add-contact',
+            success: ajaxPostSuccessCb,
+            error: ajaxPostErrorCb
+        };
+        
+        $.ajax(ajaxObj);   
+    }
+}
+
+function refreshView() {
+    var ajaxObj = {};
+    
+    ajaxObj = {
+        type: 'GET',
+        contentType: 'application/json',
+        url: 'http://localhost:3000/contact-list',
+        success: ajaxGetSuccessCb,
+        error: ajaxGetErrorCb
+    };
+
+    $.ajax(ajaxObj);
+}
+
+// ajax callbacks
+function ajaxGetSuccessCb(data) {
+    log(data);
+    showContactList(data);
+}
+
+function ajaxGetErrorCb(err) {
+    log(err);
+}
+
+function ajaxPostSuccessCb(data) {
+    refreshView();
+}
+
+function ajaxPostErrorCb(err) {
+    log(err);
+}
+
+// DOM manipulation
+function showContactList(contactList) {
+    var contactListHtml = '';
+    
+    for (var i = 0; i < contactList.length; i++) {
+        contactListHtml += 
+            '\n' + 
+            '<tr class="contactList ' + contactList[i].id + '">' +
+            '   <td>' + contactList[i].name + '</td>' +
+            '   <td>' + contactList[i].email + '</td>' +
+            '   <td>' + contactList[i].number + '</td>' +
+            '   <td>' +
+            '       <button class="btn btn-danger">Remove</button>' +
+            '   </td>' +
+            '</tr>';
+    }
+    
+    $('.contactList').remove();
+    $('tbody').append(contactListHtml);
+}
+
+// helper
+function log(data) {
+    console.log(data);
+}
--- server.js
+++ server.js
@@ -2,21 +2,86 @@
 
 // dependencies
 var express = require('express');
+var bodyParser = require('body-parser');
+var jsonfile = require('jsonfile');
 
 // server
 var app = express();
 
 // conf
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(function (err, req, res, next) {
+    console.error(err);
+    res.status(500).send(err);
+});
 
 // openshift/local server conf
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
-// routing
-//app.get('/', function(res));
+// save file config
+var file = __dirname + '/public/tmp/data.json';
+jsonfile.spaces = 4;
+
+
+// ...
+app.post('/add-contact', postCb);
+
+function postCb(req, res) {
+    var data = req.body;
+
+    jsonfile.readFile(file, readFileCb);
+
+    function readFileCb(err, obj) {
+        var dataArr = [];
+        
+        if (err) {
+            dataArr.push(data);
+            jsonfile.writeFile(file, dataArr, writeFileCb);
+            log('No data file found. Creating new data.json file in ' + file);
+
+        } else {
+            dataArr = obj;
+            dataArr.push(data);
+            jsonfile.writeFile(file, dataArr, writeFileCb);
+        }
+        
+        function writeFileCb(err) {
+            if(err) {
+                log(err);
+                res.send('Write file error!');
+            } else {
+                res.send('Data saved.');
+            }
+        }
+    }
+}
+
+
+app.get('/contact-list', getCb);
+
+function getCb(req, res, next) {
+    jsonfile.readFile(file, readFileCb);
+    
+    function readFileCb(err, obj) {
+        if (err) {
+//            res.send('getCb: error reading file');
+            log(err);
+            next(err.messаge);
+        } else {
+            res.send(obj);
+            log(obj);
+        }
+    }
+}
 
 // start app
 app.listen(port, ipaddress, function () {
     console.log('Server running on http://' + ipaddress + ':' + port);
 });
+
+// helper
+function log(data) {
+    console.log(data);
+}
